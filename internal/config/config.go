594cf6937a9190fcf59f821645fdb14908192f79
Tarcisio Oliveira <t@rcis.io>
2025-10-03 03:56:10 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,14 @@
+// Package config loads, parses, and provides the application configuration.
 package config
 
 const (
+	// Server configuration.
 	serverAddressEnvKey     = "SERVER_ADDRESS"
 	serverAddressEnvDefault = "localhost:8080"
 )
 
 type (
+	// Config holds the application configuration.
 	Config struct {
 		ServerAddress string
 	}
