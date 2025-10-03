7585fb811738a1f9d7140123e7caffa5e2760b95
Tarcisio Oliveira <t@rcis.io>
2025-10-03 04:00:04 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,10 @@
 // Package config loads, parses, and provides the application configuration.
 package config
 
+import (
+	"time"
+)
+
 const (
 	// Server configuration.
 	serverAddressEnvKey     = "SERVER_ADDRESS"
@@ -10,6 +14,22 @@ const (
 type (
 	// Config holds the application configuration.
 	Config struct {
+		// ServerAddress
 		ServerAddress string
+
+		// ServerReadTimeout
+		ServerReadTimeout time.Duration
+
+		// ServerReadHeaderTimeout
+		ServerReadHeaderTimeout time.Duration
+
+		// ServerWriteTimeout
+		ServerWriteTimeout time.Duration
+
+		// ServerIdleTimeout
+		ServerIdleTimeout time.Duration
+
+		// ServerShutdownTimeout
+		ServerShutdownTimeout time.Duration
 	}
 )
