7ac866c5af3338626f2f4c3ffec25f8a4c6e36c2
Tarcisio Oliveira <t@rcis.io>
2025-10-28 22:08:02 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,25 @@
 package config
 
+import (
+	"io"
+	"log/slog"
+)
+
 type (
+	// Config
 	Config struct {
+		// LogLevel
+		LogLevel slog.Level
+
+		// LogFormat
+		LogFormat string
+
+		// LogOutput
+		LogOutput io.Writer
 	}
 )
 
+// New
 func New() (*Config, error) {
 	return nil, nil
 }
