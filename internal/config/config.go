c4561d9255fbde0105f7e4ebceb95d33d902a1c7
Tarcisio Oliveira <t@rcis.io>
2025-10-28 22:34:39 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,6 +6,23 @@ import (
 	"log/slog"
 )
 
+// Supported log formats.
+const (
+	LogFormatJSON = "json"
+	LogFormatText = "text"
+)
+
+// Supported log outputs.
+const (
+	logOutputStdout = "stdout"
+	logOutputStderr = "stderr"
+)
+
+// Log output file mode.
+const (
+	logOutputFileMode = 0644
+)
+
 type (
 	// Config
 	Config struct {
