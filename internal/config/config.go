d1e3ff1b5efb3fddd66a85383f1af479461b2ace
Tarcisio Oliveira <t@rcis.io>
2025-11-09 18:01:50 +0000

    update

---
+++ internal/config/config.go
@@ -0,0 +1,12 @@
+package config
+
+type (
+	LogLevel string
+)
+
+const (
+	LogLevelDebug LogLevel = "debug"
+	LogLevelInfo  LogLevel = "info"
+	LogLevelWarn  LogLevel = "warn"
+	LogLevelError LogLevel = "error"
+)
