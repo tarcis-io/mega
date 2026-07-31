70e5bbb4b0b7e428b7e5248e60d19d33dee6d981
Tarcisio Oliveira <t@rcis.io>
2026-07-31 20:52:42 -0300

    update

--- cmd/mega/main.go
+++ cmd/mega/main.go
@@ -1,9 +1,13 @@
+// Package main is the entry point for the Mega application.
+//
+// It runs the primary HTTP server.
 package main
 
 import (
 	"github.com/tarcis-io/mega/internal/server"
 )
 
+// main runs the primary server responsible for handling incoming HTTP requests.
 func main() {
 	server.Run()
 }
