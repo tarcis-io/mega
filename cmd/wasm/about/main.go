2201ad5f059340b60a853a1ffd4c146deeab7f4d
Tarcisio Oliveira <t@rcis.io>
2026-08-01 15:12:36 -0300

    update

---
+++ cmd/wasm/about/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/tarcis-io/mega/internal/wasm/page/about"
+)
+
+func main() {
+	about.Render()
+}
---
+++ cmd/wasm/home/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/tarcis-io/mega/internal/wasm/page/home"
+)
+
+func main() {
+	home.Render()
+}
