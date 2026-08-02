bd5d05e29818f233111b79be820acf6bb4df8e33
Tarcisio Oliveira <t@rcis.io>
2026-08-02 12:35:27 -0300

    update

--- web/embed.go
+++ web/embed.go
@@ -1,3 +1,6 @@
+// Package web provides embedded file systems for the application's frontend resources.
+//
+// It embeds web-related assets directly into the compiled binary.
 package web
 
 import (
@@ -5,6 +8,7 @@ import (
 )
 
 var (
+	// PublicFS contains public-facing static files such as images.
 	//go:embed public
 	PublicFS embed.FS
 )
