246be5867ebd4c408d56e85a0eff55a9a090db63
Tarcisio Oliveira <t@rcis.io>
2026-08-02 16:49:59 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,28 +2,41 @@ package server
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"net"
 	"net/http"
 	"os"
+
+	"github.com/tarcis-io/mega/web"
 )
 
 const (
-	defaultPort = "8080"
+	publicDir    = "public"
+	publicPrefix = "/public/"
+	defaultPort  = "8080"
 )
 
 func Run() error {
+	publicFS, err := fs.Sub(web.PublicFS, publicDir)
+	if err != nil {
+		return fmt.Errorf("failed to initialize public file system: %w", err)
+	}
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle(publicPrefix, http.StripPrefix(publicPrefix, http.FileServer(http.FS(publicFS))))
+
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = defaultPort
 	}
 
 	addr := net.JoinHostPort(host, port)
 
-	serveMux := http.NewServeMux()
-
 	log.Printf("HTTP server is starting on http://%s", addr)
+
 	if err := http.ListenAndServe(addr, serveMux); err != nil {
 		return fmt.Errorf("HTTP server stopped unexpectedly: %w", err)
 	}
