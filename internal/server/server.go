0668dbd30f3506fd523bc02d4ec6962a42a0eef3
Tarcisio Oliveira <t@rcis.io>
2026-08-02 16:32:07 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,17 @@
 package server
 
+import (
+	"net/http"
+)
+
 func Run() error {
+	addr := ":8080"
+
+	serveMux := http.NewServeMux()
+
+	if err := http.ListenAndServe(addr, serveMux); err != nil {
+		return err
+	}
+
 	return nil
 }
