9f66c7e1f6dd6a417b7686139d53881d7232d8d9
Tarcisio Oliveira <t@rcis.io>
2026-08-02 14:11:47 -0300

    update

--- cmd/mega/main.go
+++ cmd/mega/main.go
@@ -4,10 +4,16 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/tarcis-io/mega/internal/server"
 )
 
 // main runs the primary server responsible for handling incoming HTTP requests.
 func main() {
-	server.Run()
+	if err := server.Run(); err != nil {
+		log.Printf("failed to run server: %v", err)
+		os.Exit(1)
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,3 +1,5 @@
 package server
 
-func Run() {}
+func Run() error {
+	return nil
+}
