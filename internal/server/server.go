2034cbc07b9174a88f19c65d6b98f8312fc4e885
Tarcisio Oliveira <t@rcis.io>
2026-08-02 20:01:42 -0300

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -19,34 +19,28 @@ const (
 )
 
 func Run() error {
-	router, err := setupRouter()
+	addr := buildAddr()
+
+	router, err := buildRouter()
 	if err != nil {
 		return err
 	}
 
-	addr := getServerAddress()
+	server, err := buildServer(addr, router)
+	if err != nil {
+		return err
+	}
+
 	log.Printf("HTTP server is starting on http://%s", addr)
 
-	if err := http.ListenAndServe(addr, router); err != nil {
+	if err := server.ListenAndServe(); err != nil {
 		return fmt.Errorf("HTTP server stopped unexpectedly: %w", err)
 	}
 
 	return nil
 }
 
-func setupRouter() (*http.ServeMux, error) {
-	publicFS, err := fs.Sub(web.PublicFS, publicDir)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize public file system: %w", err)
-	}
-
-	serveMux := http.NewServeMux()
-	serveMux.Handle(publicPrefix, http.StripPrefix(publicPrefix, http.FileServer(http.FS(publicFS))))
-
-	return serveMux, nil
-}
-
-func getServerAddress() string {
+func buildAddr() string {
 	host := os.Getenv("HOST")
 	if host == "" {
 		host = defaultHost
@@ -58,4 +52,25 @@ func getServerAddress() string {
 	}
 
 	return net.JoinHostPort(host, port)
+}
+
+func buildRouter() (*http.ServeMux, error) {
+	publicFS, err := fs.Sub(web.PublicFS, publicDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize public file system: %w", err)
+	}
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle(publicPrefix, http.StripPrefix(publicPrefix, http.FileServer(http.FS(publicFS))))
+
+	return serveMux, nil
+}
+
+func buildServer(addr string, handler http.Handler) (*http.Server, error) {
+	server := &http.Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+
+	return server, nil
 }
