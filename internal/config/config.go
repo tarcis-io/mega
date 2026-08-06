b86c6f4e5538b9f42f79981a1191a8c2cec3dd71
Tarcisio Oliveira <t@rcis.io>
2026-08-05 23:42:21 -0300

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,16 @@
 package config
 
+import (
+	"os"
+)
+
+const (
+	serverHostKey     = "SERVER_HOST"
+	defaultServerHost = "0.0.0.0"
+	serverPortKey     = "SERVER_PORT"
+	defaultServerPort = "8080"
+)
+
 type Config struct {
 	ServerHost string
 	ServerPort string
@@ -7,7 +18,15 @@ type Config struct {
 
 func Load() *Config {
 	return &Config{
-		ServerHost: "0.0.0.0",
-		ServerPort: "8080",
+		ServerHost: getEnv(serverHostKey, defaultServerHost),
+		ServerPort: getEnv(serverPortKey, defaultServerPort),
 	}
+}
+
+func getEnv(key, fallback string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+
+	return fallback
 }
